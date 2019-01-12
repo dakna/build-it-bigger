312506a82b07d0ff1e13cb5ea07c64111b80ca88
Daniel Knapp <opendroid@gmail.com>
2019-01-12 18:33:39 -0500

    add comments for not using local endpoint

--- app/src/main/java/app/knapp/udacity/builditbigger/GoogleCloudEndpointsAsyncTask.java
+++ app/src/main/java/app/knapp/udacity/builditbigger/GoogleCloudEndpointsAsyncTask.java
@@ -32,8 +32,12 @@ public class GoogleCloudEndpointsAsyncTask extends AsyncTask<Void, Void, String>
                 AndroidHttp.newCompatibleTransport(),
                 new AndroidJsonFactory(),
                 null)
-                .setRootUrl("https://build-it-bigger-228210.appspot.com/_ah/api/")
+
                 //.setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                // local IP was no working from emulator, maybe because I use Genymotion on an AMD system
+                // deployed to a Google Cloud Platform project works
+                .setRootUrl("https://build-it-bigger-228210.appspot.com/_ah/api/")
+
                 .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                     @Override
                     public void initialize(AbstractGoogleClientRequest<?> request) throws IOException {
